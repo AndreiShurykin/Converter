fe6bc1d4be29838441bd2a646896b11ba32003ce
AndreiShurykin <ashurykin@gmail.com>
2022-02-11 20:11:26 +0300

    Constants aded

---
+++ converter/Constants.swift
@@ -0,0 +1,23 @@
+//
+//  Constants.swift
+//  converter
+//
+//  Created by Andrei Shurykin on 10.02.22.
+//
+
+import Foundation
+
+let byn = "BYN"
+let eur = "EUR"
+let rub = "RUB"
+let usd = "USD"
+
+let currenciesSet: Set = [byn, eur, rub, usd]
+
+let urlBaseFirst = "http://free.currconv.com/api/v7/convert?q="
+let urlBaseSecond = "_"
+let urlBaseThird = "&compact=ultra&apiKey=aa1913126e854ea722b4"
+
+
+//aa1913126e854ea722b4
+//28d3abb31b66b11aec29
